9f42fdab0b0fc2e841b3dd1e239c002c0451d0af
Naufal <nauf1207@mail.ru>
2023-03-14 22:31:47 +0600

    done Task

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n;  
+int k;
+int sum=0;
+int m;
+n = Convert.ToInt32(Console.ReadLine());
+if(n > 9999 && n < 100000)
+{
+for(m= n; n != 0; n = n / 10)
+{
+k=n % 10;
+sum=sum * 10 + k;
+}
+if(m == sum)
+Console.Write($"{m} является палиндромом.");
+else
+Console.Write($"{m} не является палиндромом.");
+} else
+Console.Write("Введите пятизначное число! ");
